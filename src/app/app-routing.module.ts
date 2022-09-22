ccf4f6680f2e1073941c01717701ac0cacc02ad7
aqifcse <aqif.cse@gmail.com>
2022-09-22 20:21:34 +0600

    Routing is done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { RainfallComponent } from "./rainfall/rainfall.component";
+import { RelativeHumidityComponent } from "./relative-humidity/relative-humidity.component";
+import { TemparatureComponent } from "./temparature/temparature.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'temparatures', component: TemparatureComponent },
+    { path: 'rainfalls', component: RainfallComponent },
+    { path: 'relative-humidities', component: RelativeHumidityComponent }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
-export class AppRoutingModlue{
-}
+export class AppRoutingModule{}
+export const routingComponents = [TemparatureComponent, RainfallComponent, RelativeHumidityComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { TemparatureComponent } from './temparature/temparature.component';
-import { RainfallComponent } from './rainfall/rainfall.component';
-import { RelativeHumidityComponent } from './relative-humidity/relative-humidity.component';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TemparatureComponent,
-    RainfallComponent,
-    RelativeHumidityComponent,
+    routingComponents
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
