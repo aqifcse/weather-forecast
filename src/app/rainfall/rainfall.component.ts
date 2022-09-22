c70d1fa471547d4ad559da20026bff7a79075ad7
nzahasan <aqif.cse@gmail.com>
2022-09-22 17:57:56 +0600

    Adding Routing

--- src/app/rainfall/rainfall.component.ts
+++ src/app/rainfall/rainfall.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Rainfall } from './rainfall.model';
 
 @Component({
   selector: 'app-rainfall',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rainfall.component.css']
 })
 export class RainfallComponent implements OnInit {
+
+  rainfall: Rainfall[] = [
+    new Rainfall('2022-09-22T00:00:00', '2022-09-23T00:00:00', 65.13999938964844, 87.71, 99.55999755859375, 0.0, 0.0)
+  ];
 
   constructor() { }
 
