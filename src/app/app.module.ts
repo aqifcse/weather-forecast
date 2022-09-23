beb042aa6a3385d90f6e024685f4b9286a105500
aqifcse <aqif.cse@gmail.com>
2022-09-23 13:41:03 +0600

    Adding @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+// import { MatInput } from '@angular/material/input';
+// import { MatPaginator } from '@angular/material/paginator';
+// import { MatProgressSpinner } from '@angular/material/progress-spinner';
+// import { MatSort } from '@angular/material/sort';
+// import { MatTable } from '@angular/material/table';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,6 +23,7 @@ import { AppRoutingModule, routingComponents } from './app-routing.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
